4f4cd994cc1f5f7709e4b1dd0d4a6a032657b3e5
Matthiasclee <matthias@matthiasclee.com>
2022-01-13 13:00:56 -0800

    Updated local debug server

--- .server_assets/local_debug.rb
+++ .server_assets/local_debug.rb
@@ -3,9 +3,9 @@ def path_debug(client, data)
 		error(client, 405)
 	else
 		if data["path"] == "/__"
-			response(client, 200, ["Content-type: text/html"], File.read("./.server_assets/index.html"))
+			response(client, 200, ["Content-type: text/html"], File.read("./.server_assets/HTML/index.html"))
 		elsif data["path"] == "/__/doc"
-			response(client, 200, ["Content-type: text/html"], File.read("./.server_assets/doc.html"))
+			response(client, 200, ["Content-type: text/html"], File.read("./.server_assets/HTML/doc.html"))
 		else
 			error(client, 404)
 		end
