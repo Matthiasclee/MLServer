57f3158ccae7e2fada142e62567a6ef1e7cff26e
Matthias Lee <matthias@matthiasclee.com>
2023-01-22 17:30:04 -0800

    Added request parser

--- lib/MLserver/request_parser.rb
+++ lib/MLserver/request_parser.rb
@@ -1,4 +1,36 @@
 module MLserver
   module RequestParser
+    def parse_request(client)
+      keepReading = true
+      headers = {}
+      data = ""
+
+      req = client.gets
+
+      #Close if bad request
+      if req.to_s.length < 3
+        #close(client)
+        #Thread.exit
+      end
+
+      #Get all headers
+      while keepReading do
+        x = client.gets
+        if x.chomp.length == 0
+          keepReading = false
+        else
+          begin
+            headers[x.split(": ")[0]] = x.split(": ")[1].chomp
+          rescue => error
+            error(client, 500, error)
+          end
+        end
+      end
+
+      #Get payload data
+      data = client.read(headers["Content-Length"].to_i)
+
+      return Request.new(headers: headers, request: req, data: data)
+    end
   end
 end
