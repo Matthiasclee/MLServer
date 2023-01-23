773066c683d07ed2aa0af11c75e27b2bef5ce379
Matthias Lee <matthias@matthiasclee.com>
2023-01-22 17:24:16 -0800

    Added request

--- lib/MLserver/request.rb
+++ lib/MLserver/request.rb
@@ -1,5 +1,25 @@
 module MLserver
   class Request
+    def initialize(headers: {}, request:, data: "")
+      @headers = headers
+      @request = request
+      @data = data
 
+      @request_split = @request.split(" ")
+    end
+
+    def method
+      @request_split[0]
+    end
+
+    def path
+      @request_split[1]
+    end
+
+    def httpver
+      @request_split[2]
+    end
+
+    attr_reader :headers, :request, :data
   end
 end
