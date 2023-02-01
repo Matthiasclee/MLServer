d0808d2ff3b356aaf2caefdb91c4a9deb667dd8d
Matthias Lee <matthias@matthiasclee.com>
2023-02-01 14:48:36 -0800

    server runs in a loop

--- lib/MLserver/server.rb
+++ lib/MLserver/server.rb
@@ -3,9 +3,12 @@ module MLserver
     def self.start(host: "0.0.0.0", port: 80)
       server = TCPServer.new(host, port)
 
-      x=server.accept
-      r=RequestParser.parse_request(x)
-      x.puts Response.new(data: "a").to_s
+      loop do
+        Thread.start(server.accept) do |client|
+          r=RequestParser.parse_request(client)
+          client.puts Response.new(data: "a").to_s
+        end
+      end
     end
   end
 end
