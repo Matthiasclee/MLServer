06bdb04a67c3247ea46180383677d5296e8e9397
Matthias Lee <matthias@matthiasclee.com>
2023-02-02 06:37:31 -0800

    Server disconnects on http2 request

--- lib/MLserver/server.rb
+++ lib/MLserver/server.rb
@@ -10,6 +10,19 @@ module MLserver
       loop do
         Thread.start(server.accept) do |client|
           r=RequestParser.parse_request(client)
+
+          if r.httpver != "HTTP/1.0" || httpver != "HTTP/1.1"
+            client_ip = client.peeraddr[2]
+
+            resp = MLserver::ErrorResponse.new(505)
+            client.puts resp.response.to_s
+            client.close
+
+            logger.log_err "Closed connection from #{client_ip}: Unsupported HTTP method (#{r.httpver})"
+
+            Thread.exit
+          end
+
           logger.log "#{client.peeraddr[2]} => #{r.method} #{r.path}"
           handler.run(r, client)
         end
