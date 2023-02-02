61494b4d2584ed4e0ba28f1ce42154fd223b10c4
Matthias Lee <matthias@matthiasclee.com>
2023-02-01 20:30:53 -0800

    Server outputs more logs

--- lib/MLserver/server.rb
+++ lib/MLserver/server.rb
@@ -1,7 +1,11 @@
 module MLserver
   module Server
     def self.start(host: "0.0.0.0", port: 80, handler:, logger:)
+      logger.log "MLserver #{MLserver.version}"
+
       server = TCPServer.new(host, port)
+
+      logger.log "Listening on #{host}:#{port}"
 
       loop do
         Thread.start(server.accept) do |client|
